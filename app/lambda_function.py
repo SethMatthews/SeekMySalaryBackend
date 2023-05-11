394039b25cb0f8e93e5972678e98f82980844ca7
Seth Matthews <sethmatthews@Seths-MacBook-Pro-2.local>
2023-05-12 08:49:30 +1000

    added new headers

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -117,11 +117,16 @@ def lambda_handler(event, context):
     return {
         'statusCode': 200,
         'body': json.dumps(output_salary_range),
-        "headers":{
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin" : 'http://127.0.0.1:5500/'
+        # "headers":{
+        #     "Content-Type": "application/json",
+        #     "Access-Control-Allow-Origin" : "http://127.0.0.1:5500/"
+        # }
+        'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
         }
-    }
+        }
 
 # Lambda must be configured with Python 3.9 runtime
 
