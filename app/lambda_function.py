83eeab26b6e8e0d8722a008a705855394f14284f
Seth Matthews <sethmatthews@Seths-MacBook-Pro-2.local>
2023-05-29 14:50:16 +1000

    'Access-Control-Allow-Origin': 'https://seekmysalary.com', change from *

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -160,7 +160,7 @@ def lambda_handler(event, context):
         # }
         'headers': {
             'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Origin': 'https://seekmysalary.com',
             'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
         }
         }
